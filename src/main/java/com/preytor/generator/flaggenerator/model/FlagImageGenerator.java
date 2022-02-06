454ed939db177c31221ddaf2931ef93b0f9e2e5c
preytor <pgrfpgm@gmail.com>
2022-02-06 23:00:03 +0100

    Changed all the images from indexed to rgb Added the ability to change color of the division_field

--- src/main/java/com/preytor/generator/flaggenerator/model/FlagImageGenerator.java
+++ src/main/java/com/preytor/generator/flaggenerator/model/FlagImageGenerator.java
@@ -1,18 +1,24 @@
 package com.preytor.generator.flaggenerator.model;
 
+import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
+import java.awt.image.WritableRaster;
+import java.awt.Image;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
-import java.io.OutputStream;
 import java.io.UncheckedIOException;
 import java.util.Base64;
 
 import javax.imageio.ImageIO;
 
 public class FlagImageGenerator {
-    
+
+    private final int newW = 320;
+    private final int newH = 480;
+
     private BufferedImage final_image;
     private Flag flag;
 
@@ -34,15 +40,13 @@ public class FlagImageGenerator {
                 try {
                     img_division_field = ImageIO.read(new File(division_directory));
                 } catch (IOException e1) {
-                    System.out.println("Enters here?????? WHY");
                     //e1.printStackTrace();
                 }
             }
-
-            Graphics g = final_image.getGraphics();
+            img_division_field = colorImage(img_division_field, new Color(255, 0, 255));//new Color(255, 0, 255));//new Color(255, 255, 255));
+            Graphics2D g = final_image.createGraphics();
             g.drawImage(img_division_field, 0, 0, null);
             g.dispose();
-            System.out.println("did the image");
         }
         if (this.flag.getFurs() > 0){
 
@@ -53,7 +57,8 @@ public class FlagImageGenerator {
         if (this.flag.getHeraldic_charges() > 0){
 
         }
-        
+
+        this.resize(final_image, newW, newH);
     }
 
     public String imgToBase64String() {
@@ -66,6 +71,33 @@ public class FlagImageGenerator {
         }
     }
 
+    public void resize(BufferedImage bufferedImage, int newW, int newH) { 
+        Image tmp = bufferedImage.getScaledInstance(newW, newH, Image.SCALE_SMOOTH);
+        BufferedImage dimg = new BufferedImage(newW, newH, BufferedImage.TYPE_INT_ARGB);
     
+        Graphics2D g2d = dimg.createGraphics();
+        g2d.drawImage(tmp, 0, 0, null);
+        g2d.dispose();
+    
+        this.final_image = dimg;
+    }
+
+    private static BufferedImage colorImage(BufferedImage image, Color color) {
+        int width = image.getWidth();
+        int height = image.getHeight();
+        for (int xx = 0; xx < width; xx++) {
+            for (int yy = 0; yy < height; yy++) {
+                int rgbpixel = image.getRGB(xx,yy);
+                if( (rgbpixel>>24) != 0x00 ) {
+                    image.setRGB(xx, yy, color.getRGB());//.setPixel(xx, yy, pixel);//.setSample(xx,yy,b,pixel[b]); 
+                }
+            }
+        }
+        return image;
+    }
+
+    public BufferedImage getFinal_image() {
+        return final_image;
+    }
 
 }

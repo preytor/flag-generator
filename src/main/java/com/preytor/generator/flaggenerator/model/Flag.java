1b8ed3e332148eebde8d585c452b94e27439f874
preytor <pgrfpgm@gmail.com>
2022-02-16 01:03:20 +0100

    Added 3 more colors so you can change all of the parts of the flag

--- src/main/java/com/preytor/generator/flaggenerator/model/Flag.java
+++ src/main/java/com/preytor/generator/flaggenerator/model/Flag.java
@@ -5,6 +5,9 @@ public class Flag {
     private String color1;
     private String color2;
     private String color3;
+    private String color4;
+    private String color5;
+    private String color6;
     
     private int division_field = 1;
     private int furs = 0;
@@ -13,11 +16,14 @@ public class Flag {
 
     public Flag(){}
 
-    public Flag(String color1, String color2, String color3, int division_field, int furs, int cadency,
+    public Flag(String color1, String color2, String color3, String color4, String color5, String color6, int division_field, int furs, int cadency,
             int heraldic_charges) {
         this.color1 = color1;
         this.color2 = color2;
         this.color3 = color3;
+        this.color4 = color4;
+        this.color5 = color5;
+        this.color6 = color6;
         this.division_field = division_field;
         this.furs = furs;
         this.cadency = cadency;
@@ -46,6 +52,30 @@ public class Flag {
 
     public void setColor3(String color3) {
         this.color3 = color3;
+    }
+
+    public String getColor4() {
+        return color4;
+    }
+
+    public void setColor4(String color4) {
+        this.color4 = color4;
+    }
+
+    public String getColor5() {
+        return color5;
+    }
+
+    public void setColor5(String color5) {
+        this.color5 = color5;
+    }
+
+    public String getColor6() {
+        return color6;
+    }
+
+    public void setColor6(String color6) {
+        this.color6 = color6;
     }
 
     public int getDivision_field() {
@@ -83,6 +113,7 @@ public class Flag {
     @Override
     public String toString() {
         return "Flag [cadency=" + cadency + ", color1=" + color1 + ", color2=" + color2 + ", color3=" + color3
+                + ", color4=" + color4 + ", color5=" + color5 + ", color6=" + color6
                 + ", division_field=" + division_field + ", furs=" + furs + ", heraldic_charges=" + heraldic_charges
                 + "]";
     }

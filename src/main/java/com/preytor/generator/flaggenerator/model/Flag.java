b31a1dead12dc19474380f4d5d1230c3ae89998a
preytor <pgrfpgm@gmail.com>
2022-02-17 13:59:47 +0100

    Added a method to count all the files and added some protection so they can't select a file/color that doesn't exist

--- src/main/java/com/preytor/generator/flaggenerator/model/Flag.java
+++ src/main/java/com/preytor/generator/flaggenerator/model/Flag.java
@@ -1,81 +1,97 @@
 package com.preytor.generator.flaggenerator.model;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public class Flag {
     
-    private String color1;
-    private String color2;
-    private String color3;
-    private String color4;
-    private String color5;
-    private String color6;
+    private PredefinedColors color1;
+    private PredefinedColors color2;
+    private PredefinedColors color3;
+    private PredefinedColors color4;
+    private PredefinedColors color5;
     
     private int division_field = 1;
     private int furs = 0;
     private int cadency = 0;
     private int heraldic_charges = 0;
 
+    private int TOTAL_DIVISION_FIELD = 0;
+    private int TOTAL_FURS = 0;
+    private int TOTAL_CADENCY = 0;
+    private int TOTAL_HERALDIC_CHARGES = 0;
+
     public Flag(){}
 
-    public Flag(String color1, String color2, String color3, String color4, String color5, String color6, int division_field, int furs, int cadency,
+    public Flag(int color1, int color2, int color3, int color4, int color5, int division_field, int furs, int cadency,
             int heraldic_charges) {
-        this.color1 = color1;
-        this.color2 = color2;
-        this.color3 = color3;
-        this.color4 = color4;
-        this.color5 = color5;
-        this.color6 = color6;
-        this.division_field = division_field;
-        this.furs = furs;
-        this.cadency = cadency;
-        this.heraldic_charges = heraldic_charges;
+
+        this.TOTAL_DIVISION_FIELD = countFilesInFolder("division_field");
+        this.TOTAL_FURS = countFilesInFolder("furs");
+        this.TOTAL_CADENCY = countFilesInFolder("cadency");
+        this.TOTAL_HERALDIC_CHARGES = countFilesInFolder("heraldic_charges");   
+
+        this.color1 = PredefinedColors.getPredefinedColors(color1);
+        this.color2 = PredefinedColors.getPredefinedColors(color2);
+        this.color3 = PredefinedColors.getPredefinedColors(color3);
+        this.color4 = PredefinedColors.getPredefinedColors(color4);
+        this.color5 = PredefinedColors.getPredefinedColors(color5);
+        
+        this.setDivision_field(division_field);
+        this.setFurs(furs);
+        this.setCadency(cadency);
+        this.setHeraldic_charges(heraldic_charges);
     }
 
-    public String getColor1() {
+    private int countFilesInFolder(String type){
+        long type_length = 0;
+        try {
+            type_length = Files.list(Paths.get("src/main/resources/assets/"+type+"/")).count();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return (int) type_length;
+    }
+
+    public PredefinedColors getColor1() {
         return color1;
     }
 
-    public void setColor1(String color1) {
+    public void setColor1(PredefinedColors color1) {
         this.color1 = color1;
     }
 
-    public String getColor2() {
+    public PredefinedColors getColor2() {
         return color2;
     }
 
-    public void setColor2(String color2) {
+    public void setColor2(PredefinedColors color2) {
         this.color2 = color2;
     }
 
-    public String getColor3() {
+    public PredefinedColors getColor3() {
         return color3;
     }
 
-    public void setColor3(String color3) {
+    public void setColor3(PredefinedColors color3) {
         this.color3 = color3;
     }
 
-    public String getColor4() {
+    public PredefinedColors getColor4() {
         return color4;
     }
 
-    public void setColor4(String color4) {
+    public void setColor4(PredefinedColors color4) {
         this.color4 = color4;
     }
 
-    public String getColor5() {
+    public PredefinedColors getColor5() {
         return color5;
     }
 
-    public void setColor5(String color5) {
+    public void setColor5(PredefinedColors color5) {
         this.color5 = color5;
-    }
-
-    public String getColor6() {
-        return color6;
-    }
-
-    public void setColor6(String color6) {
-        this.color6 = color6;
     }
 
     public int getDivision_field() {
@@ -83,7 +99,12 @@ public class Flag {
     }
 
     public void setDivision_field(int division_field) {
-        this.division_field = division_field;
+        this.division_field = 0;
+
+        long division_field_length = TOTAL_DIVISION_FIELD;
+        if (division_field >= 0 && division_field <= division_field_length){
+            this.division_field = division_field;
+        }
     }
 
     public int getFurs() {
@@ -91,7 +112,12 @@ public class Flag {
     }
 
     public void setFurs(int furs) {
-        this.furs = furs;
+        this.furs = 0;
+
+        int furs_length = TOTAL_FURS;
+        if (furs >= 0 && furs <= furs_length){
+            this.furs = furs;
+        }
     }
 
     public int getCadency() {
@@ -99,7 +125,12 @@ public class Flag {
     }
 
     public void setCadency(int cadency) {
-        this.cadency = cadency;
+        this.cadency = 0;
+
+        int cadency_length = TOTAL_CADENCY;
+        if (cadency >= 0 && cadency <= cadency_length){
+            this.cadency = cadency;
+        }
     }
 
     public int getHeraldic_charges() {
@@ -107,13 +138,34 @@ public class Flag {
     }
 
     public void setHeraldic_charges(int heraldic_charges) {
-        this.heraldic_charges = heraldic_charges;
+        this.heraldic_charges = 0;
+
+        int heraldic_charges_length = TOTAL_HERALDIC_CHARGES;
+        if (heraldic_charges >= 0 && heraldic_charges <= heraldic_charges_length){
+            this.heraldic_charges = heraldic_charges;
+        }
+    }
+
+    public int getTotal_division_field() {
+        return TOTAL_DIVISION_FIELD;
+    }
+
+    public int getTotal_furs() {
+        return TOTAL_FURS;
+    }
+
+    public int getTotal_cadency() {
+        return TOTAL_CADENCY;
+    }
+    
+    public int getTotal_heraldic_charges() {
+        return TOTAL_HERALDIC_CHARGES;
     }
 
     @Override
     public String toString() {
         return "Flag [cadency=" + cadency + ", color1=" + color1 + ", color2=" + color2 + ", color3=" + color3
-                + ", color4=" + color4 + ", color5=" + color5 + ", color6=" + color6
+                + ", color4=" + color4 + ", color5=" + color5
                 + ", division_field=" + division_field + ", furs=" + furs + ", heraldic_charges=" + heraldic_charges
                 + "]";
     }
